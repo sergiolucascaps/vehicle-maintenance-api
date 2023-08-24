5ac9c75538defb26f06695f7f96015ca8d5e510b
Sergio Lucas <sergiolucascaps@hotmail.com>
2023-08-23 21:55:52 -0300

    Added EF Core 7 and Mapping User File @sergiolucascaps

---
+++ src/SL.VehicleMaintenance.Infrastructure/Mappings/UserMapping.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SL.VehicleMaintenance.Domain.Models;
+
+namespace SL.VehicleMaintenance.Infrastructure.Mappings
+{
+	public class UserMapping : IEntityTypeConfiguration<User>
+	{
+		public void Configure(EntityTypeBuilder<User> builder)
+		{
+			builder.HasKey(c => c.Id);
+
+			builder.Property(c => c.CreatedDate)
+				.IsRequired(true);
+
+			builder.Property(c => c.UpdatedDate)
+				.IsRequired(false);
+
+			builder.Property(c => c.IsActive)
+				.IsRequired(true);
+
+			builder.Property(c => c.IsDeleted)
+				.IsRequired(true);
+
+			// 1 : N => User : Phones
+			builder.HasMany(c => c.Phones)
+				.WithOne(p => p.User)
+				.HasForeignKey(p => p.UserId);
+
+			// 1 : N => User : Emails
+			builder.HasMany(c => c.Emails)
+				.WithOne(p => p.User)
+				.HasForeignKey(p => p.UserId);
+
+			// 1 : N => User : Vehicles
+			builder.HasMany(c => c.Vehicles)
+				.WithOne(p => p.User)
+				.HasForeignKey(p => p.UserId);
+
+			builder.ToTable("Users");
+		}
+	}
+}
